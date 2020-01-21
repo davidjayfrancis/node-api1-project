8bdd068da6272ca43d95f83026620642bfbcd582
David Francis <davidjayfrancis@gmail.com>
2020-01-20 21:35:51 -0800

    built a react app around the server/database with ability to add and delete users through a basic UI

---
+++ friendster/src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+import AddFriendForm from "./components/AddFriendForm.js";
+
+function App() {
+  const [friends, setFriends] = React.useState([]);
+
+  const addFriend = friendInfo => {
+    axios({
+      method: "post",
+      url: "http://127.0.0.1:5000/api/users",
+      data: friendInfo
+    })
+      .then(res => {
+        fetchFriends();
+      })
+      .catch(err => console.log(err));
+  };
+
+  const handleDelete = e => {
+    axios({
+      method: "delete",
+      url: "http://127.0.0.1:5000/api/users/:id",
+      data: { id: e.target.id }
+    })
+      .then(res => fetchFriends())
+      .catch(err => console.log(err));
+  };
+
+  const fetchFriends = () => {
+    axios
+      .get("http://127.0.0.1:5000/api/users")
+      .then(users => {
+        setFriends(users.data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  React.useEffect(() => {
+    fetchFriends();
+  }, []);
+
+  return (
+    <div className="App">
+      <p>Friend's Capacitator</p>
+      <AddFriendForm addFriend={addFriend} />
+      {friends.map(friend => {
+        return (
+          <div>
+            <p>
+              {friend.name}: {friend.bio}
+            </p>
+            <button id={friend.id} onClick={handleDelete}>
+              Delete user
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
